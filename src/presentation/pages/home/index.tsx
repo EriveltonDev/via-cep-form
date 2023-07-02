f5de3e8c04c38e2207ac1253a453a0104e381551
EriveltonDev <eriveltondesousa26@gmail.com>
2023-07-02 11:51:17 -0300

    chore: eslint config

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -1,28 +1,28 @@
 'use client'
 
-import { Address } from "@/domain/usecases/address"
-import { useState } from "react"
+import { Address } from '@/domain/usecases/address'
+import { useState } from 'react'
 
 interface HomeProps {
   remoteAddress: Address
 }
 
-export function Home({remoteAddress}: HomeProps) {
-  const [cep, setCep] = useState('')
+export function Home({ remoteAddress }: HomeProps) {
+	const [cep, setCep] = useState('')
 
-  async function getZipCode() {
-    try {
-      const response = await remoteAddress.getByZipCode({zipCode: cep})
-      alert(JSON.stringify(response))
-    } catch (error) {
-      alert(error)
-    }
-  }
+	async function getZipCode() {
+		try {
+			const response = await remoteAddress.getByZipCode({ zipCode: cep })
+			alert(JSON.stringify(response))
+		} catch (error) {
+			alert(error)
+		}
+	}
 
-  return (
-    <>
-      <input onChange={(e) => setCep(e.target.value)}/>
-      <button onClick={getZipCode}>Buscar</button>
-    </>
-  )
+	return (
+		<>
+			<input onChange={(e) => setCep(e.target.value)}/>
+			<button onClick={getZipCode}>Buscar</button>
+		</>
+	)
 }
